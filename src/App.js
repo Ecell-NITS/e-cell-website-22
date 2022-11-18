c69db624ca25825421fdf9ac99a4d907eee2bf2d
Aditya <kumar.31aditya@gmail.com>
2022-11-19 02:14:20 +0530

    final event done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
+import Events from './components/Home/Event/Events';
 import GalleryPage from './pages/GalleryPage/GalleryPage';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
             <Route exact path="/gallery" element={< GalleryPage />} key='route-gallery-screen' />
           </Routes>
       </BrowserRouter>
+      <Events/>
   </>
   );
 }
---
+++ src/components/Home/Event/Events.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Events.css'
+function Events() {
+  return (
+    <>
+      <div className="event_main">
+        <div className="heading"><h1>EVENTS</h1></div>
+        <div className="event_items">
+          <div className="ev item1">
+            <h1 className='item-head'>ORIENTATION</h1>
+            <div className="child">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat a alias nemo voluptates. Quae, eum exercitationem? Nobis, ad omnis! Totam vitae, harum illo explicabo dolores, ipsa aliquid ad officiis nemo aperiam ea ullam repellendus? Suscipit perferendis maiores nostrum veritatis sequi!</div>
+          </div>
+          <div className="ev item2">
+            <h1 className='item-head'>EPRESERRIO</h1>
+            <div className="child">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat a alias nemo voluptates. Quae, eum exercitationem? Nobis, ad omnis! Totam vitae, harum illo explicabo dolores, ipsa aliquid ad officiis nemo aperiam ea ullam repellendus? Suscipit perferendis maiores nostrum veritatis sequi!</div>
+          </div>
+          <div className=" ev item3">
+            <h1 className='item-head'>SRIJAN</h1>
+            <div className="child">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat a alias nemo voluptates. Quae, eum exercitationem? Nobis, ad omnis! Totam vitae, harum illo explicabo dolores, ipsa aliquid ad officiis nemo aperiam ea ullam repellendus? Suscipit perferendis maiores nostrum veritatis sequi!</div>
+          </div>
+          <div className="ev item4 ">
+            <h1 className='item-head'>INCUBATION</h1>
+            <div className="child">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat a alias nemo voluptates. Quae, eum exercitationem? Nobis, ad omnis! Totam vitae, harum illo explicabo dolores, ipsa aliquid ad officiis nemo aperiam ea ullam repellendus? Suscipit perferendis maiores nostrum veritatis sequi!</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Events
