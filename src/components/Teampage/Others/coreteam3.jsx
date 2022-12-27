085f933554b94095e05e6d47a5ef297fc31f33b7
Dimpal Kalita <kalitadimpal112@gmail.com>
2022-12-28 01:31:45 +0530

    Added core team and developer section

--- src/components/Teampage/Others/coreteam3.jsx
+++ src/components/Teampage/Others/coreteam3.jsx
@@ -1,13 +1,42 @@
 import React from 'react'
+import '../../css/Core.css'
+import { FaFacebook, FaLinkedin } from 'react-icons/fa'
+import { BsGithub } from 'react-icons/bs'
+import Data from '../../Core.json'
 
-function coreteam3() {
-  return (
-    <div>
-      <h1>2020-2021</h1>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus est, distinctio itaque dolor, unde error quam voluptatum expedita quo quod assumenda neque dolores labore necessitatibus molestiae dignissimos incidunt corporis vel.
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore a impedit praesentium corporis earum culpa veritatis nam harum sint, odit suscipit esse iusto ullam excepturi. Tempore dolorem et iusto sint.
-    </div>
-  )
+function Core(){
+return(
+<>
+<section className="core-team">
+      <div className="heading">
+         <h1>Core Team</h1>
+         {/* <h2>2020-2021</h2> */}
+      </div>
+   <div className="container">
+   {
+      Data.map(data=>{
+         return (
+         <div className="core-mem" key={data.id}>
+         <div className="body">
+            <div className="images">
+               <img src={data.image} alt="" />
+               </div>
+            <h3 className='title'>{data.name}</h3>
+         </div>
+         <p className="text">{data.rank}</p>
+         <div className="social-media">
+            <a href={data.fb} className="socialMediaLogo"><FaFacebook size={30}/></a>
+            <a href={data.linkedln} className="socialMediaLogo"><FaLinkedin size={30}/></a> 
+            <a href={data.git} className="socialMediaLogo"><BsGithub size={30}/></a>
+         </div>
+      </div>
+         )
+      })
+   }
+</div>
+</section>
+</>
+)
 }
 
-export default coreteam3
+export default Core;
