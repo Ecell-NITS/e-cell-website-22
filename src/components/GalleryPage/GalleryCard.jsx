487917ae8b3e2fbeaccea5051d62743579d5def9
Dimpal Kalita <kalitadimpal112@gmail.com>
2022-12-31 01:25:28 +0530

    changed one photo link

--- src/components/GalleryPage/GalleryCard.jsx
+++ src/components/GalleryPage/GalleryCard.jsx
@@ -4,10 +4,10 @@ function GalleryCard(props) {
   return (
     <div>
         <div className="card">
-            <img src={props.imgsrc} alt="img" className="card-img-top" />
             <div className="card-body">
                 <h5 className="card-title">{props.id}</h5>
              </div>
+            <img src={props.imgsrc} alt="img" className="card-img-top" />
             </div>           
     </div>
   )
