24cfe274cbffead8b9eae628655ad85ace8f7028
hereisadi <hereisadi@proton.me>
2023-01-12 14:37:05 +0530

    about us bug fix

--- src/components/Home/About Us/About_us.jsx
+++ src/components/Home/About Us/About_us.jsx
@@ -24,21 +24,15 @@ function About_us() {
 
         <div className="about_text_div">
           <p className="about_text">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor minus
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id totam
-            exercitationem earum et quis repellat dicta nihil eveniet veritatis.
-            Reprehenderit quas veniam impedit minus hic adipisci repudiandae,
-            molestias totam blanditiis velit autem ab dolore nam at quia dolorem
-            officiis, culpa incidunt. Explicabo eaque officiis eveniet dolorum
-            natus adipisci consequatur ab hic magnam unde, nesciunt, qui debitis
-            odit perferendis laborum voluptatum? Cumque, sunt! Ut, dolorum
-            voluptatibus. Sed adipisci dolore, Lorem, ipsum dolor sit amet
-            consectetur adipisicing elit. Rem doloremque earum architecto harum
-            ipsam? Cumque molestiae qui tenetur? Error, nesciunt.
+          Lorem ipsum dolor sit amet consectetur. Malesuada mauris auctor nisl pellentesque facilisis ornare euismod sit. Fermentum velit cras sed nullam maecenas tincidunt gravida fringilla augue. Dignissim tellus scelerisque a sem penatibus lacus diam quis. Tempus dui enim neque pretium posuere blandit. Cras nullam vulputate eleifend vitae enim morbi adipiscing. Habitasse vestibulum eget sed sed risus sed sed. Pellentesque pulvinar id non sed scelerisque odio commodo tincidunt egestas. Purus hac rutrum id cursus posuere. Vitae nulla elit risus ut potenti. Arcu ultricies volutpat urna ridiculus interdum vulputate mattis. <br/><br/>
+Malesuada nisi mauris est feugiat aliquet. Elementum facilisis at senectus in nisi sagittis. Leo in sed arcu neque at. Venenatis congue sit eu fringilla commodo tellus in vitae. Nisl cras sit nunc neque sem mauris id tempor laoreet. Odio urna amet nullam dictum. Ut aliquet scelerisque at vulputate sem sit egestas. Nunc felis aenean lacus pulvinar nunc dis aliquet. Arcu dui gravida sit ipsum fermentum dictum faucibus lorem. Donec ut eget tellus tincidunt odio malesuada quisque hendrerit nibh. Etiam nunc libero lobortis arcu vitae turpis enim urna. Sed accumsan in tempor risus felis diam enim tellus blandit. Urna iaculis elementum est pretium fusce a lacus. Morbi eget tristique netus enim imperdiet lobortis nulla ultricies.
           </p>
         </div>
+
       </div>
+
       {/* for cards */}
+
       <div className="motto">
         <h3 style={{ fontSize: "64px" }}>MOTTO</h3>
       </div>
