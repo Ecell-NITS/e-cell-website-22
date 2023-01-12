33aa3be9fec7e96a0d804c18d4f310768b82d858
Aditya <kumar.31aditya@gmail.com>
2023-01-13 01:19:29 +0530

    bug fix faculties & alumni

--- src/components/Teampage/Alumni/Alumni.jsx
+++ src/components/Teampage/Alumni/Alumni.jsx
@@ -1,35 +1,40 @@
 import React, { useState } from 'react'
 import './Alumni.css'
-import Data from '../../../Data/Faculties.json';
-import ModalCard from '../Alumni/Modal/ModalCard';
+import Data from '../../../Data/Alumni.json'
+import ModalCard from '../Alumni/Modal/ModalCardAlumni';
 import { Modal, ModalBody, ModalHeader } from "reactstrap"
 function Alumni() {
-  const [modal, setmodal] = useState(false)
-  return (
-    <>
-      <section className="alumni">
-        <div className="cards">
-          {
-            Data.map(data => {
-              return (<div key={data.id} className="profile" onClick={() => setmodal(true)}>
-                <div className="img" ><img src={data.image} alt="Profile of author" /></div>
-                <div className="name">{data.name}</div>
-                <div className="name1">{data.rank}</div>
-              </div>
+    const [modal, setmodal] = useState(false)
+    const [modalid, setmodalid] = useState(1);
+    return (
+        <>
+            <section className="alumni">
+                <div className="cards">
+                    {
+                        Data.map(data => {
+                            return (
+                            <div key={data.id} className="profile" onClick={() => {
+                                setmodalid(data.id)
+                                setmodal(true)
+                                }}>
+                                <div className="img_alumni" ><img src={data.image} alt="Profile of author" /></div>
+                                <div className="name_alumni">{data.name}</div>
+                                <div className="desg_alumni">{data.rank}</div>
+                            </div>
 
-              )
-            })
-          }
-        </div>
-        <Modal size='lg' isOpen={modal} toggle={() => setmodal(!modal)}>
-          <ModalHeader toggle={() => setmodal(!modal)}></ModalHeader>
-          <ModalBody>
-            <ModalCard />
-          </ModalBody>
-        </Modal>
-      </section>
-    </>
-  )
+                            )
+                        })
+                    }
+                </div>
+                <Modal size='lg' isOpen={modal} toggle={() => setmodal(!modal)}>
+                    <ModalHeader toggle={() => setmodal(!modal)}></ModalHeader>
+                    <ModalBody>
+                        <ModalCard dataid={modalid}/>                        
+                    </ModalBody>
+                </Modal>
+            </section>
+        </>
+    )
 }
 
 export default Alumni
