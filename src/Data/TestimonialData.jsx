f66e0e138e89617214df6f5b1a0b26bdccef457d
hereisadi <hereisadi@proton.me>
2023-01-19 21:54:15 +0530

    ADD: collab data + UPD: improve testimonials

---
+++ src/Data/TestimonialData.jsx
@@ -0,0 +1,30 @@
+export const testimonials = [
+    {
+        id:1,
+        img:"https://www.linkpicture.com/q/sivaji.jpg",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsum consequatur nemo earum voluptatem tenetur, beatae accusamus facere soluta accusantium voluptatibus, reprehenderit a ratione suscipit. Debitis ab voluptas dignissimos incidunt maxime dolorum sed, odio, facere quia, nobis voluptates maiores eum placeat quaerat! Cum ratione molestias odit nam sint? Voluptatum, nostrum aspernatur dolores quos illum repellat eaque placeat maiores, ducimus odit ut quia similique, iure quaerat inventore reiciendis? Laborum possimus magni molestiae reiciendis nemo quos eveniet modi cumque voluptates! Itaque delectus ut, quam adipisci quaerat nulla! Provident voluptate delectus reprehenderit sit, ratione quibusdam quaerat facere in itaque error porro, sapiente perspiciatis!",
+        prof:"Prof. Shivaji Bandhopadhyay",
+        post:"Director, NIT Silchar",
+    },
+    {
+        id:2,
+        img:"https://www.linkpicture.com/q/sivaji.jpg",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsum consequatur nemo earum voluptatem tenetur, beatae accusamus facere soluta accusantium voluptatibus, reprehenderit a ratione suscipit. Debitis ab voluptas dignissimos incidunt maxime dolorum sed, odio, facere quia, nobis voluptates maiores eum placeat quaerat! Cum ratione molestias odit nam sint? Voluptatum, nostrum aspernatur dolores quos illum repellat eaque placeat maiores, ducimus odit ut quia similique, iure quaerat inventore reiciendis? Laborum possimus magni molestiae reiciendis nemo quos eveniet modi cumque voluptates! Itaque delectus ut, quam adipisci quaerat nulla! Provident voluptate delectus reprehenderit sit, ratione quibusdam quaerat facere in itaque error porro, sapiente perspiciatis!",
+        post:"Director, NIT Silchar",
+        prof:"Prof. Shivaji Bandhopadhyay",
+    },
+    {
+        id:3,
+        img:"https://www.linkpicture.com/q/sivaji.jpg",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsum consequatur nemo earum voluptatem tenetur, beatae accusamus facere soluta accusantium voluptatibus, reprehenderit a ratione suscipit. Debitis ab voluptas dignissimos incidunt maxime dolorum sed, odio, facere quia, nobis voluptates maiores eum placeat quaerat! Cum ratione molestias odit nam sint? Voluptatum, nostrum aspernatur dolores quos illum repellat eaque placeat maiores, ducimus odit ut quia similique, iure quaerat inventore reiciendis? Laborum possimus magni molestiae reiciendis nemo quos eveniet modi cumque voluptates! Itaque delectus ut, quam adipisci quaerat nulla! Provident voluptate delectus reprehenderit sit, ratione quibusdam quaerat facere in itaque error porro, sapiente perspiciatis!",
+        prof:"Prof. Shivaji Bandhopadhyay",
+        post:"Director, NIT Silchar",
+    },
+    {
+        id:4,
+        img:"https://www.linkpicture.com/q/sivaji.jpg",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. At ipsum consequatur nemo earum voluptatem tenetur, beatae accusamus facere soluta accusantium voluptatibus, reprehenderit a ratione suscipit. Debitis ab voluptas dignissimos incidunt maxime dolorum sed, odio, facere quia, nobis voluptates maiores eum placeat quaerat! Cum ratione molestias odit nam sint? Voluptatum, nostrum aspernatur dolores quos illum repellat eaque placeat maiores, ducimus odit ut quia similique, iure quaerat inventore reiciendis? Laborum possimus magni molestiae reiciendis nemo quos eveniet modi cumque voluptates! Itaque delectus ut, quam adipisci quaerat nulla! Provident voluptate delectus reprehenderit sit, ratione quibusdam quaerat facere in itaque error porro, sapiente perspiciatis!",
+        prof:"Prof. Shivaji Bandhopadhyay",
+        post:"Director, NIT Silchar",
+    },
+];
