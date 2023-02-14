eee32168532b657e50635c8a2e3c5969684835e3
Aditya <kumar.31aditya@gmail.com>
2023-02-15 02:58:45 +0530

    ADD: Resource Page (Complete)

--- src/Data/ResourcesData.jsx
+++ src/Data/ResourcesData.jsx
@@ -2,35 +2,59 @@
 export const resourcesdta = [
     {
         id:1,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:2,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:3,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:4,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:5,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:6,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:7,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     },
     {
         id:8,
-        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg"
+        head:"Connect The Dots",
+        author:"Author - Rasmi Bansal",
+        img:"https://res.cloudinary.com/dp92qug2f/image/upload/c_scale,w_360,f_auto,fl_lossy/v1676397123/resources-ecell/book-demo_gaszyn.jpg",
+        content:"Lorem first dolor sit amet consectetur adipisicing elit. Nam magnam saepe fuga molestias, eaque tenetur rerum vero debitis eveniet eos culpa quod repudiandae assumenda eligendi autem obcaecati sed? Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit, commodi iste deleniti quam illo corrupti rem possimus dicta vitae necessitatibus! Atque consequuntur earum alias tempore obcaecati quod quaerat repellat minima impedit!"
     }
 ]
 
--- src/pages/Resources/Resources.jsx
+++ src/pages/Resources/Resources.jsx
@@ -1,92 +1,119 @@
-import React, { useRef } from 'react'
-import Footer from '../../components/shared/Footer/Footer'
-import NavbarTeam from '../../components/shared/Navbar/NavbarTeam'
-import { resourcesdta } from '../../Data/ResourcesData'
-import { podcastdata } from '../../Data/ResourcesData'
-import './Resources.css';
-import { AiOutlineArrowRight, AiOutlineArrowLeft } from 'react-icons/ai'
+import React, { useRef, useState } from "react";
+import Footer from "../../components/shared/Footer/Footer";
+import NavbarTeam from "../../components/shared/Navbar/NavbarTeam";
+import { resourcesdta } from "../../Data/ResourcesData";
+import { podcastdata } from "../../Data/ResourcesData";
+import "./Resources.css";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+import { Modal, ModalBody, ModalHeader } from "reactstrap";
+import BookModal from "../../components/ResourceModal/BookModal";
+import PodcastModal from "../../components/ResourceModal/PodcastModal";
+
 const Resources = () => {
+  const [modal, setmodal] = useState(false);
+  const [modalid, setmodalid] = useState(1);
+  let resourcesHolder = useRef();
 
-    let resourcesHolder = useRef();
+  function fwdNext() {
+    resourcesHolder.current.scrollLeft += resourcesHolder.current.offsetWidth;
+  }
 
-    function fwdNext() {
-        resourcesHolder.current.scrollLeft += (resourcesHolder.current.offsetWidth);
-    };
+  function backPrev() {
+    resourcesHolder.current.scrollLeft -= resourcesHolder.current.offsetWidth;
+  }
 
-    function backPrev() {
-        resourcesHolder.current.scrollLeft -= (resourcesHolder.current.offsetWidth);
-    };
+  let podcastHolder = useRef();
 
-    let podcastHolder = useRef();
+  function fwdNext0() {
+    podcastHolder.current.scrollLeft += podcastHolder.current.offsetWidth;
+  }
 
-    function fwdNext0() {
-        podcastHolder.current.scrollLeft += (podcastHolder.current.offsetWidth);
-    };
+  function backPrev0() {
+    podcastHolder.current.scrollLeft -= podcastHolder.current.offsetWidth;
+  }
 
-    function backPrev0() {
-        podcastHolder.current.scrollLeft -= (podcastHolder.current.offsetWidth);
-    };
+  return (
+    <>
+      <NavbarTeam />
+      <div className="resources-main">
+        <h1>Resources</h1>
+      </div>
 
-    return (
-        <>
-            <NavbarTeam />
-            <div className="resources-main">
-                <h1>Resources</h1>
-            </div>
+      <div className="collab">
+        <h1 style={{ userSelect: "none" }}>BOOKS SUMMARY</h1>
+      </div>
 
-            <div className="collab">
-                <h1 style={{ userSelect: 'none' }} >BOOKS SUMMARY</h1>
-            </div>
+      <div className="resources-indi" ref={resourcesHolder}>
+        {resourcesdta.map((item) => {
+          return (
+            <>
+              <div
+                className="resource-indi"
+                key={item.id}
+                onClick={() => {
+                  setmodalid(item.id);
+                  setmodal(true);
+                }}
+              >
+                <div className="img-resour">
+                  <img src={item.img} alt="" />
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+      <div className="btns">
+        <button className="prev btn-testimonial" onClick={backPrev}>
+          <AiOutlineArrowLeft className="btn-indi-testimonial" />
+        </button>
+        <button className="next btn-testimonial" onClick={fwdNext}>
+          <AiOutlineArrowRight className="btn-indi-testimonial" />
+        </button>
+      </div>
 
-            <div className="resources-indi" ref={resourcesHolder}>
-                {resourcesdta.map((item) => {
-                    return (
-                        <>
-                            <div className="resource-indi" key={item.id}>
-                                <div className="img-resour">
-                                    <img src={item.img} alt="" />
-                                </div>
-                            </div>
-                        </>
-                    )
-                })}
-            </div>
-            <div className="btns">
-                <button className="prev btn-testimonial" onClick={backPrev}>
-                    <AiOutlineArrowLeft className="btn-indi-testimonial" />
-                </button>
-                <button className="next btn-testimonial" onClick={fwdNext}>
-                    <AiOutlineArrowRight className="btn-indi-testimonial" />
-                </button>
-            </div>
+      <div className="collab">
+        <h1 style={{ userSelect: "none" }}>PODCAST SUMMARY</h1>
+      </div>
+      <div className="resources-indi" ref={podcastHolder}>
+        {podcastdata.map((item) => {
+          return (
+            <>
+              <div className="resource-indi hjloq" key={item.id} onClick={() => {
+                  setmodalid(item.id);
+                  setmodal(true);
+                }}>
+                <div className="img-resour">
+                  <img src={item.img} alt="" />
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </div>
+      <div className="btns">
+        <button className="prev btn-testimonial" onClick={backPrev0}>
+          <AiOutlineArrowLeft className="btn-indi-testimonial" />
+        </button>
+        <button className="next btn-testimonial" onClick={fwdNext0}>
+          <AiOutlineArrowRight className="btn-indi-testimonial" />
+        </button>
+      </div>
+      <Modal size="lg" isOpen={modal} toggle={() => setmodal(!modal)}>
+        <ModalHeader toggle={() => setmodal(!modal)}></ModalHeader>
+        <ModalBody>
+          <BookModal dataid={modalid} />
+        </ModalBody>
+      </Modal>
+      <Modal size="lg" isOpen={modal} toggle={() => setmodal(!modal)}>
+        <ModalHeader toggle={() => setmodal(!modal)}></ModalHeader>
+        <ModalBody>
+          <PodcastModal dataid={modalid} />
+        </ModalBody>
+      </Modal>
+      <Footer />
+    </>
+  );
+};
 
-            <div className="collab">
-                <h1 style={{ userSelect: 'none' }} >PODCAST SUMMARY</h1>
-            </div>
-            <div className="resources-indi" ref={podcastHolder}>
-                {podcastdata.map((item) => {
-                    return (
-                        <>
-                            <div className="resource-indi hjloq" key={item.id}>
-                                <div className="img-resour">
-                                    <img src={item.img} alt="" />
-                                </div>
-                            </div>
-                        </>
-                    )
-                })}
-            </div>
-            <div className="btns">
-                <button className="prev btn-testimonial" onClick={backPrev0}>
-                    <AiOutlineArrowLeft className="btn-indi-testimonial" />
-                </button>
-                <button className="next btn-testimonial" onClick={fwdNext0}>
-                    <AiOutlineArrowRight className="btn-indi-testimonial" />
-                </button>
-            </div>
-            <Footer />
-        </>
-    )
-}
-
-export default Resources
+export default Resources;
