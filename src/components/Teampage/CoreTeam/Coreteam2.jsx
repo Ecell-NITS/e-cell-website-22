c16c87db1a5570c71a2d11447af25d3d41442c38
2116025_Maharnav Deka <dekamaharnav200@gmail.com>
2023-02-21 15:55:23 +0530

    link bug fix

--- src/components/Teampage/CoreTeam/Coreteam2.jsx
+++ src/components/Teampage/CoreTeam/Coreteam2.jsx
@@ -25,7 +25,7 @@ function CoreTeam1() {
          <p className="core_mem_text">{data.rank}</p>
          <div className="social-media">
             <a href={data.fb} className="SocialLogo"><FaFacebook size={30}/></a>
-            <a href={data.linkedln} className="SocialLogo"><FaLinkedin size={30}/></a> 
+            <a href={data.linkdln} className="SocialLogo"><FaLinkedin size={30}/></a> 
             <a href={data.git} className="SocialLogo"><BsGithub size={30}/></a>
          </div>
       </div>
