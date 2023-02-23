d9d72b2e5fda55334a7178ef5e60aa202692f1c5
Priyanshu Jayant <priyanshujayant3121@gmail.com>
2023-02-24 00:40:54 +0530

    Srijan Event Page Created

--- src/components/Home/Events/Events.jsx
+++ src/components/Home/Events/Events.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Events.css";
+import { useNavigate } from 'react-router-dom';
 function Events() {
+
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/events_red` ; 
+    navigate(path);
+  }
   return (
     <>
       <div className="event_main">
@@ -31,7 +38,7 @@ function Events() {
               module to bring out the true start-up geek in students.
             </div>
           </div>
-          <div className=" ev item3">
+          <div className=" ev item3" onClick={()=>routeChange()}>
             <h1 className="item-head">SRIJAN</h1>
             <div className="child">
               Srijan is the flagship event of the Entrepreneurship Cell of NIT
