d463498ed39153573babac0af978f0ca4c5877b0
Priyajit27 <priyajitpaul27@gmail.com>
2023-02-25 12:34:19 -0800

    Sponsors section added

--- src/pages/Events_red/Events_red.jsx
+++ src/pages/Events_red/Events_red.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import IndividualAboutUs from '../../components/EventsRed/IndividualAboutUs/IndividualAboutUs'
 import IndividualHero from '../../components/EventsRed/IndividualHero/IndividualHero'
 import NavbarTeam from '../../components/shared/Navbar/NavbarTeam'
-
+import Sponsor from '../../components/EventsRed/Sponsor/Sponsor'
 const Events_red = () => {
   return (
     <div>
       <NavbarTeam/>
       <IndividualHero/>
       <IndividualAboutUs/>
+      <Sponsor/>
     </div>
   )
 }
