2a57242dfd4629eb18b7ae3e11853b62af52016e
Biju Borah <biju20_ug@ee.nits.ac.in>
2023-02-28 23:38:11 +0530

    initial commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,75 @@ import Pillars from '../../components/Home/Pillars_of_Ecell/Pillars'
 import Timeline from '../../components/Home/Timeline/Timeline'
 import Footer from '../../components/shared/Footer/Footer'
 import Footerconstant from '../../components/shared/FooterConstant/Footerconstant'
+import Chatbot from 'react-chatbot-kit'
+import 'react-chatbot-kit/build/main.css'
+import { createChatBotMessage } from 'react-chatbot-kit';
+import Hello from './hello'
+const config = {
+  initialMessages: [createChatBotMessage(`Hi I'm Ecell Bot and I'm here to help you. What do you want to know?`,
+    { widget: "options", delay: 500 }),
+  ],
+  widgets: [
+    {
+      widgetName: "options",
+      widgetFunc: (props) => <Hello{...props} />,
+    },
+  ],
+};
+
+const messages = ["Name", "alumini name", "question", "Email", "Message"];
+var index = 0;
+
+const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+  const processMessage = () => {
+    var message;
+    if (index == messages.length) message = createChatBotMessage('Thank you for your response. We will get back to you soon. Have a nice day.', { delay: 500 })
+    else message = createChatBotMessage(messages[index], { delay: 500 })
+    setState((prevState) => ({
+      ...prevState,
+      messages: [...prevState.messages, message],
+    }));
+  };
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {
+            processMessage
+          },
+        });
+      })}
+    </div>
+  );
+};
+var data = {};
+
+const MessageParser = ({ children, actions }) => {
+  const parse = (message) => {
+    actions.processMessage();
+    if (typeof (message) == "object") index++;
+    else if (index < messages.length) {
+      data[messages[index - 1]] = message;
+      index++;
+    }
+    else console.log(data);
+  };
+
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          parse: parse,
+          actions: {
+            parse
+          },
+        });
+      })}
+    </div>
+  );
+};
+
+
 const Home = () => {
   useEffect(() => {
     document.title = "ECELL | NITS";
@@ -23,6 +92,11 @@ const Home = () => {
       <Testimonial />
       <Footerconstant />
       <Footer />
+      <Chatbot
+        config={config}
+        messageParser={MessageParser}
+        actionProvider={ActionProvider}
+      />
     </>
   )
 }
---
+++ src/pages/Home/hello.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./hello.css";
+
+const Hello = (props) => {
+    const options = [
+        { text: "questions", handler: props.actions.parse, id: 1 },
+        { text: "Contact us", handler: () => { }, id: 2 },
+    ];
+
+    const optionsMarkup = options.map((option) => (
+        <button
+            className="learning-option-button"
+            key={option.id}
+            onClick={option.handler}
+        >
+            {option.text}
+        </button>
+    ));
+
+    return <div className="learning-options-container">{optionsMarkup}</div>;
+};
+
+export default Hello;
