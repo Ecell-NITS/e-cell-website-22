9c2d95a245e82628e24b0a35d0391193d8fae364
hereisadi <hereisadi@proton.me>
2023-03-31 03:30:51 +0530

    UPD:timeline assets fetch from cloudinary

--- src/components/Home/Timeline/Timeline.jsx
+++ src/components/Home/Timeline/Timeline.jsx
@@ -1,12 +1,6 @@
 import React from 'react'
 import './Timeline.css'
-import orientation from '../../../assets/Orientation.png'
-import arrowup from '../../../assets/arrow-up.png'
-import arrowdown from '../../../assets/arrow-down.png'
-import eic from '../../../assets/Innovation.png'
-import srjn from '../../../assets/Srijan.png'
-import empressario from '../../../assets/Empressario.png'
-import preencu from '../../../assets/Pre-Incubation.png'
+
 function Timeline() {
   return (
     <div className='timeline-section'>
@@ -17,94 +11,94 @@ function Timeline() {
       <div className="timeline-main">
         <div className="timeline-sub">
           <div className="timeline-child bnjkop" id='orie'>
-            <img src={orientation} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212643/Ecell%20website/timeline/Orientation_gaytqe.webp" alt="" />
             <h1 className='child-title'>Orientation</h1>
           </div>
 
           <div className="timeline-child tc2 down-reverse" id='ar-up'>
-            <img className="arrows" src={arrowup} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213056/Ecell%20website/timeline/arrow-up_bmxbml.webp" alt="" />
           </div>
 
           <div className="timeline-child bnjkop" id='eic'>
-            <img src={eic} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212808/Ecell%20website/timeline/Innovation_gxtyjk.webp" alt="" />
             <h1 className='child-title'>EIC</h1>
           </div>
 
           <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
-            <img className="arrows" src={arrowdown} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213077/Ecell%20website/timeline/arrow-down_ao2bls.webp" alt="" />
           </div>
 
           <div className="timeline-child child-up tc4 bnjkop" id="srj ">
-            <img src={srjn} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212878/Ecell%20website/timeline/Srijan_n0rrqe.webp" alt="" />
             <h1 className='child-title' id="srj-title">Srijan</h1>
           </div>
 
           <div className="timeline-child tc2 tc2impo" id='ar-up'>
-            <img className="arrows" src={arrowup} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213056/Ecell%20website/timeline/arrow-up_bmxbml.webp" alt="" />
           </div>
 
           <div className="timeline-child tc4 bnjkop" id="emp ">
-            <img src={empressario} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212949/Ecell%20website/timeline/Empressario_fb9fkt.webp" alt="" />
             <h1 className='child-title'>Empressario</h1>
           </div>
 
           <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
-            <img className="arrows" src={arrowdown} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213077/Ecell%20website/timeline/arrow-down_ao2bls.webp" alt="" />
           </div>
 
           <div className="timeline-child tc4 tc-pre bnjkop" id="prb ">
-            <img src={preencu} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213019/Ecell%20website/timeline/Pre-Incubation_ya5otw.webp" alt="" />
             <h1 className='child-title preinvunmkj' id="prb-title ">Pre-Incubation</h1>
           </div>
 
           <div className="timeline-child tc2 down-reverse" id='ar-up'>
-            <img className="arrows" src={arrowup} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213056/Ecell%20website/timeline/arrow-up_bmxbml.webp" alt="" />
           </div>
 
 
           <div className="timeline-child bnjkop" id='orie'>
-            <img src={orientation} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212643/Ecell%20website/timeline/Orientation_gaytqe.webp" alt="" />
             <h1 className='child-title'>Orientation</h1>
           </div>
 
           <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
-            <img className="arrows" src={arrowdown} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213077/Ecell%20website/timeline/arrow-down_ao2bls.webp" alt="" />
           </div>
 
           <div className="timeline-child bnjkop" id='eic'>
-            <img src={eic} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212808/Ecell%20website/timeline/Innovation_gxtyjk.webp" alt="" />
             <h1 className='child-title'>EIC</h1>
           </div>
 
           <div className="timeline-child tc2 tc2impo" id='ar-up'>
-            <img className="arrows" src={arrowup} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213056/Ecell%20website/timeline/arrow-up_bmxbml.webp" alt="" />
           </div>
 
           <div className="timeline-child child-up tc4 bnjkop" id="srj ">
-            <img src={srjn} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212878/Ecell%20website/timeline/Srijan_n0rrqe.webp" alt="" />
             <h1 className='child-title' id="srj-title">Srijan</h1>
           </div>
 
           <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
-            <img className="arrows" src={arrowdown} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213077/Ecell%20website/timeline/arrow-down_ao2bls.webp" alt="" />
           </div>
 
           <div className="timeline-child tc4 bnjkop" id="emp ">
-            <img src={empressario} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680212949/Ecell%20website/timeline/Empressario_fb9fkt.webp" alt="" />
             <h1 className='child-title'>Empressario</h1>
           </div>
 
           <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
-            <img className="arrows" src={arrowdown} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213077/Ecell%20website/timeline/arrow-down_ao2bls.webp" alt="" />
           </div>
 
           <div className="timeline-child tc4 tc-pre bnjkop" id="prb ">
-            <img src={preencu} alt="" />
+            <img src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213019/Ecell%20website/timeline/Pre-Incubation_ya5otw.webp" alt="" />
             <h1 className='child-title preinvunmkj' id="prb-title ">Pre-Incubation</h1>
           </div>
 
           <div className="timeline-child tc2 down-reverse" id='ar-up'>
-            <img className="arrows" src={arrowup} alt="" />
+            <img className="arrows" src="https://res.cloudinary.com/dp92qug2f/image/upload/v1680213056/Ecell%20website/timeline/arrow-up_bmxbml.webp" alt="" />
           </div>
         </div>
         
