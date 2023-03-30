c6f7cc2cccba587dcf1d00dd7da6c271a0e2c944
hereisadi <hereisadi@proton.me>
2023-03-30 22:28:59 +0530

    UPD:timeline

--- src/components/Home/Timeline/Timeline.jsx
+++ src/components/Home/Timeline/Timeline.jsx
@@ -56,6 +56,47 @@ function Timeline() {
             <img src={preencu} alt="" />
             <h1 className='child-title preinvunmkj' id="prb-title ">Pre-Incubation</h1>
           </div>
+
+          <div className="timeline-child bnjkop" id='orie'>
+            <img src={orientation} alt="" />
+            <h1 className='child-title'>Orientation</h1>
+          </div>
+
+          <div className="timeline-child tc2 down-reverse" id='ar-up'>
+            <img className="arrows" src={arrowup} alt="" />
+          </div>
+
+          <div className="timeline-child bnjkop" id='eic'>
+            <img src={eic} alt="" />
+            <h1 className='child-title'>EIC</h1>
+          </div>
+
+          <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
+            <img className="arrows" src={arrowdown} alt="" />
+          </div>
+
+          <div className="timeline-child child-up tc4 bnjkop" id="srj ">
+            <img src={srjn} alt="" />
+            <h1 className='child-title' id="srj-title">Srijan</h1>
+          </div>
+
+          <div className="timeline-child tc2 tc2impo" id='ar-up'>
+            <img className="arrows" src={arrowup} alt="" />
+          </div>
+
+          <div className="timeline-child tc4 bnjkop" id="emp ">
+            <img src={empressario} alt="" />
+            <h1 className='child-title'>Empressario</h1>
+          </div>
+
+          <div className="timeline-child tc2 tc3 mid-ar" id='ar-dn'>
+            <img className="arrows" src={arrowdown} alt="" />
+          </div>
+
+          <div className="timeline-child tc4 tc-pre bnjkop" id="prb ">
+            <img src={preencu} alt="" />
+            <h1 className='child-title preinvunmkj' id="prb-title ">Pre-Incubation</h1>
+          </div>
         </div>
         
       </div>
